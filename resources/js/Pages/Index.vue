797abf280fe8028dc66b8b4409661b670fead914
aashishhhhhh <47654151+aashishhhhhh@users.noreply.github.com>
2023-07-16 21:06:44 -0400

    Starter Pack

---
+++ resources/js/Pages/Index.vue
@@ -0,0 +1,20 @@
+<template>
+    Hy {{ name }}
+    <Link href="nextPage"> askdj
+    </Link>
+</template>
+
+<script>
+import { Link } from '@inertiajs/vue3'
+export default {
+    components: {
+        Link
+    },
+    props: {
+        name: String
+    },
+    data() {
+        return {};
+    },
+};
+</script>
